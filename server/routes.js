2feeac890c71b6eb9ca9d1848a76a769a79d1666
Chris Dhanaraj <adhanaraj@gmail.com>
2016-06-04 14:48:50 -0500

    point user api to user api folder

--- server/routes.js
+++ server/routes.js
@@ -4,8 +4,10 @@ module.exports = (app) => {
 
   // api routes
   app.use('/api/apiary', require('./api/apiary'));
+  // app.use('/api/hive', require('./api/hive'));
+  // app.use('/api/inspection', require('./api/inspection'));
 
-
+  app.use('/api/user', require('./api/user'));
 
   // react routes
   app.use('/dashboard/*', (req, res) => {
