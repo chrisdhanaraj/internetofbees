cb154f7603b1e436038defc704dc89f3f3584f69
Chris Dhanaraj <adhanaraj@gmail.com>
2016-06-04 15:10:08 -0500

    actually add the inspection file

---
+++ server/api/inspection/inspection.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const inspectionSchema = Schema({
+  hive: {
+    type: Schema.Types.ObjectId,
+    ref: 'Hive',
+  },
+  _id: Number,
+  name: String,
+  date: Date,
+  temperature: Number,
+  traffic: String,
+  beesCrawlingOnGround: Boolean,
+  beesBringingPollen: Boolean,
+  feederInPlace: Boolean,
+  numberFramesSealedBrood: Number,
+  numberFramesOpenBrood: Number,
+  seeOpenDisease: Boolean,
+  seeSmallLarvae: Boolean,
+  seeOpenNectar: Boolean,
+  totalFramesBees: Number,
+  totalFramesBrood: Number,
+  totalFramesHoney, Number,
+  numberDeepBoxes: Number,
+  numberMediumBoxes: Number,
+  numberBadCombs: Number,
+  seeQueen: Boolean,
+  seeQueenCells: Boolean,
+  seeDrones: Boolean,
+  temperament: String,
+  medicationsAdded: Boolean,
+  medicationTypes: [],
+  bottomBoard: String,
+  comments: String,
+  pictureLink: String,
+  inspectionType: String,
+  actionsTaken: String,
+  actionsNeeded: String,
+  nextInspectionDate: Date,
+  queenRight: Boolean,
+  beeDensity: String,
+  foodStores: String,
+  pests: String,
+  diseases: String,
+  humidity: String,
+  weight: Number,
+  honeyFramesHarvested: Number,
+  broodFramesHarvested: Number
+});
+
+const Inspection = mongoose.model('Inspection', inspectionSchema);
