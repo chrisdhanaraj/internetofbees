6f45879a5e61ff4d41ea07bdf24f9695d747651c
hilaryd <hadyer100@yahoo.com>
2016-06-04 14:19:45 -0500

    Update index.js

--- server/api/apiary/index.js
+++ server/api/apiary/index.js
@@ -5,7 +5,8 @@ var apiarySchema = Schema({
   _id     : Number,
   userId    : Number,
   name     : String,
-  location : String
+  location : String,
+  hives : [{ type: Schema.Types.ObjectId, ref: 'Hive' }]
 });
 
 var Apiary  = mongoose.model('Apiary', apiarySchema);
