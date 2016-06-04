9148668b301e385f86dcd68d6e76371a55ea9b86
Chris Dhanaraj <adhanaraj@gmail.com>
2016-06-04 16:26:45 -0500

    bring in full user

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -44,12 +44,12 @@ const signin = (req, res) => {
  };
 
 const signup = (req, res) => {
-  const user = Object.assign({}, req.body, {
+  const userObj = Object.assign({}, req.body, {
     password: password.hashPassword(req.body.password),
   });
 
 
-  User.create(user, (err) => {
+  User.create(userObj, (err, user) => {
     if (err) {
       throw err;
     }
